31c94a43b60182c4f02fff37a25ed0f820fcdb46
Denis <dmel01@icloud.com>
2022-11-30 18:31:13 +0400

    Stage 2 completed

--- Encryption-Decryption/task/src/encryptdecrypt/EncryptService.java
+++ Encryption-Decryption/task/src/encryptdecrypt/EncryptService.java
@@ -1,17 +1,39 @@
 package encryptdecrypt;
 
 public class EncryptService {
-    public static String encryptText(String text) {
-        char[] textChrArr = text.toCharArray();
-        char[] encryptedArr = new char[textChrArr.length];
-        for (int i = 0; i < textChrArr.length; i++) {
-            if (String.valueOf(textChrArr[i]).matches("\\w")) {
-                int diff = (textChrArr[i] - 'a');
-                encryptedArr[i] = (char) ('z' - diff);
+    private static final String[] ALPHABETARRAY = "abcdefghijklmnopqrstuvwxyz".split("");
+
+    public static String encryptText(String text, int key) {
+
+        String[] textArr = text.split("");
+        String[] encryptedArr = new String[textArr.length];
+        for (int i = 0; i < textArr.length; i++) {
+            if (String.valueOf(textArr[i]).matches("\\w")) {
+                int indexNewLetter = getNewLetterIndex(textArr[i], key);
+                encryptedArr[i] = ALPHABETARRAY[indexNewLetter];
             } else {
-                encryptedArr[i] = textChrArr[i];
+                encryptedArr[i] = textArr[i];
             }
         }
-        return new String(encryptedArr);
+        return String.join("", encryptedArr);
+    }
+
+    private static int getLetterIndex(String letter) {
+        int index = 0;
+        for (int i = 0; i < ALPHABETARRAY.length; i++) {
+            if (ALPHABETARRAY[i].contains(letter)) {
+                return i;
+            }
+        }
+        return index;
+    }
+
+    private static int getNewLetterIndex(String letter, int key) {
+        int oldIndex = getLetterIndex(letter);
+        int newIndex = oldIndex + key;
+        while (ALPHABETARRAY.length < newIndex) {
+            newIndex -= ALPHABETARRAY.length;
+        }
+        return newIndex;
     }
 }
--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,7 +1,12 @@
 package encryptdecrypt;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println(EncryptService.encryptText("we found a treasure!"));
+        Scanner scanner = new Scanner(System.in);
+        String text = scanner.nextLine();
+        int key = scanner.nextInt();
+        System.out.println(EncryptService.encryptText(text, key));
     }
 }
