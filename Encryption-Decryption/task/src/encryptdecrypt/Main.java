ebde949c3eb2c6da86ba63ed314895830a524901
Denis <dmel01@icloud.com>
2022-12-01 15:50:15 +0400

    Stage 4 completed

--- Encryption-Decryption/task/src/encryptdecrypt/Main.java
+++ Encryption-Decryption/task/src/encryptdecrypt/Main.java
@@ -1,14 +1,20 @@
 package encryptdecrypt;
 
-import java.util.Scanner;
-
 public class Main {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        String typeOperation = scanner.nextLine();
-        String text = scanner.nextLine();
-        int key = scanner.nextInt();
-        switch (typeOperation) {
+        String mode = "";
+        int key = 0;
+        String text = "";
+        for (int i = 0; i < args.length; i += 2) {
+            if (args[i].contains("-mode")) {
+                mode = args[i + 1];
+            } else if (args[i].contains("-key")) {
+                key = Integer.parseInt(args[i + 1]);
+            } else if (args[i].contains("-data")) {
+                text = args[i + 1];
+            }
+        }
+        switch (mode) {
             case "enc" -> System.out.println(EncryptDecryptionService.encryptText(text, key));
             case "dec" -> System.out.println(EncryptDecryptionService.decryptText(text, key));
             default -> System.out.println("Wrong input");
